4ba85ae9927df88e80ff132d2c52da7eb0cbe85d
kyoviet97 <kyoviet97@gmail.com>
2020-10-22 13:37:26 +0700

    Commit file socket

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const port = process.env.PORT || 5000;
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/index.html');
+});
+
+//Whenever someone connects this gets executed
+io.on('connection', function (socket) {
+   console.log('A user connected');
+   // var token = socket.request._query
+   // console.log("=======================>>>> token: ", token.auth_token);
+   // console.log("=======================>>>> idClient: ", socket.id);
+   // // if (token.auth_token == undefined) {
+   //    io.sockets.sockets[socket.id].disconnect();
+   // }
+
+   socket.on('message_client', function (data) {
+      io.emit('message_server', data);
+   });
+
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
+
+
+http.listen(port, function () {
+   console.log('listening on *:5000');
+});
